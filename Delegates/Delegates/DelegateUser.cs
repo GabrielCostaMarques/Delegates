32791fbcd0fa1982fbf61d20e8a5008ab5fdd3bc
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-03-18 00:41:28 -0300

    Add project files.

---
+++ Delegates/Delegates/DelegateUser.cs
@@ -0,0 +1,40 @@
+﻿using CourseCSharp3.Delegates.Entities;
+
+namespace CourseCSharp3.Delegates
+{
+    internal class DelegateUser
+    {
+        public void DelegateShow()
+        {
+            List<Product> list = [];
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+
+            //fazendo com lambda, sem ter que declarar uma função
+            //Comparison<Product> comp=(p1,p2)=>p1.Name.ToLower().CompareTo(p2.Name.ToLower());
+
+            ////////////////////////
+            
+
+            //usando lambda inline
+            list.Sort((p1, p2) => p1.Name.ToLower().CompareTo(p2.Name.ToLower()));
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+        //Delegate sendo criado, pode ser referenciado como list.Sort(CompareProducts)
+
+        //static int CompareProducts(Product p1, Product p2)
+        //{
+        //    return p1.Name.ToLower().CompareTo(p2.Name.ToLower());
+        //}
+    }
+}
---
+++ Delegates/DelegatesV2/DelegatesVTwoUser.cs
@@ -0,0 +1,29 @@
+﻿using Course.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCSharp3.DelegatesV2
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal class DelegatesVTwoUser
+    {
+        public void DelegatesVTwo()
+        {
+            double a = 10;
+            double b = 20;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            //invoke para invocar os paramâmetros da função maxs
+
+            Console.WriteLine(op.Invoke(a, b));
+
+
+        }
+    }
+}
