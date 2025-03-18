9444e666573abf045b7b49680e299a7357926f04
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2025-03-18 00:56:18 -0300

    Delegates multicasting

---
+++ Delegates/MultiCastingDelegates/MultiCastingDelegatesUser.cs
@@ -0,0 +1,22 @@
+﻿using Course.Services;
+using Delegates.MultiCastingDelegates;
+
+namespace Delegates.MultiCastingDelegates
+{
+    delegate void BinaryNumericOperantion(double n1, double n2);
+    internal class MultiCastingDelegatesUser
+    {
+        public void MultiCastingDelegatesShow()
+        {
+            double a = 10;
+            double b = 55;
+
+            BinaryNumericOperantion op = MultiCastingDelegatesServices.ShowSum;
+
+            //multicasting, mostrando a soma e o máximo
+            op += MultiCastingDelegatesServices.ShowMax;
+
+            op.Invoke(b, a);
+        }
+    }
+}
